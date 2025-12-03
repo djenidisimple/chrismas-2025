1d87e9b3f9010de4630e1a236bb254d754aa33f7
djenidisimple <djenidigauss@gmail.com>
2025-12-03 11:22:00 +0300

    add snowflakes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext("2d");
-let x = 0;
+let snowflakes = [];
 const snow = (x, y) => {
     ctx.beginPath();
     ctx.arc(x, y, 3, 0, Math.PI * 2);
@@ -9,11 +9,28 @@ const snow = (x, y) => {
     ctx.closePath();
 }
 
+for (let i = 0; i < 50; i++) {
+    snowflakes.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        speed: Math.random() * 2 + 1
+    });
+}
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-for (let i = 10; i <= 1000; i += 45) {
-    snow(i, 10)
-}
-
+const id = setInterval(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    snowflakes.forEach(flake => {
+        snow(flake.x, flake.y);
+        flake.x += Math.sin(flake.y * 0.01) * 0.5;
+        flake.y += flake.speed;
+        
+        if (flake.y > canvas.height) {
+            flake.y = 0;
+            flake.x = Math.random() * canvas.width;
+        }
+    });
+}, 50);
 
